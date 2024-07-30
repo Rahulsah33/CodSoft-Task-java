6a3454dd500f27f7ce0a941e2e2999f1b201e97a
Rahul Kumar sah <146347529+Rahulsah33@users.noreply.github.com>
2024-07-30 20:53:14 +0530

    Create Rules.java

---
+++ QuizeApp/Rules.java
@@ -0,0 +1,73 @@
+package quizeapplication;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Rules extends JFrame implements ActionListener {
+
+    String name;
+    JButton start, back;
+
+    Rules(String name) {
+        this.name = name;
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+
+        JLabel heading = new JLabel("Welcome " + name + " to Simple Minds");
+        heading.setBounds(50, 20, 700, 30);
+        heading.setFont(new Font("Viner Hand ITC", Font.BOLD, 28));
+        heading.setForeground(new Color(30, 144, 254));
+        add(heading);
+
+        JLabel rules = new JLabel();
+        rules.setBounds(20, 90, 700, 350);
+        rules.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        rules.setText(
+            "<html>" + 
+            "1. You have 20 minutes to complete the quiz.<br><br>" +
+            "2. Each question has 4 options, only one of which is correct.<br><br>" +
+            "3. Each question has 15 seconds.<br><br>" +
+            "4. You can skip a question and come back to it later.<br><br>" +
+            "5. Each correct answer awards you 1 point.<br><br>" +
+            "6. There is no negative marking for wrong answers.<br><br>" +
+            "7. Make sure to submit your quiz before time runs out.<br><br>" +
+            "8. Use the 'Next' button to move to the next question.<br><br>" +
+            "9. Click 'Submit' when you have completed the quiz.<br><br>" +
+            "</html>"
+        );
+        add(rules);
+
+        back = new JButton("Back");
+        back.setBounds(250, 500, 100, 30);
+        back.setBackground(new Color(30, 144, 254));
+        back.setForeground(Color.WHITE);
+        back.addActionListener(this);
+        add(back);
+
+        start = new JButton("Start");
+        start.setBounds(400, 500, 100, 30);
+        start.setBackground(new Color(30, 144, 254));
+        start.setForeground(Color.WHITE);
+        start.addActionListener(this);
+        add(start);
+
+        setSize(800, 650);
+        setLocation(350, 100);
+        setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getSource() == start) {
+            setVisible(false);
+            new Quiz(name);
+        } else {
+            setVisible(false);
+            new Login();
+        }
+    }
+
+    public static void main(String[] args) {
+        new Rules("User");
+    }
+}
